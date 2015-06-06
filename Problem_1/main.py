1e3b1683852ee65ff3d8256b4d24041a1c74e15f
Romain SOMMERARD <rsommerard@gmail.com>
2015-06-06 08:19:05 +0200

    Problem 1 solved

---
+++ Problem_1/main.py
@@ -0,0 +1,13 @@
+def main():
+    sum = 0
+
+    for i in range(0, 1000):
+        if i % 3 == 0:
+            sum += i
+        elif i % 5 == 0:
+            sum += i
+
+    print(sum)
+
+if __name__ == '__main__':
+    main()
